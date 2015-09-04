a86ac5f5ec63e106f77d774bc6361434df55aa55
Andrew Oppenlander <andrew.oppenlander@zipscene.com>
2015-09-04 10:38:47 -0400

    Saving/removing ES Documents now works

--- test/aggregates.js
+++ test/aggregates.js
@@ -1,4 +1,4 @@
-const expect = require('chai').expect;
+// const { expect } = require('chai');
 const testUtils = require('./lib/test-utils');
 
 describe.skip('Aggregates', function() {
--- test/model.js
+++ test/model.js
@@ -1,4 +1,4 @@
-const expect = require('chai').expect;
+// const { expect } = require('chai');
 
 const testUtils = require('./lib/test-utils');
 const { ElasticsearchModel } = require('../lib');
@@ -11,7 +11,7 @@ describe('ElasticsearchModel', function() {
 	it('should create models and initialize them through the index', function() {
 		this.timeout(10000);
 		let Animal = new ElasticsearchModel('Animal', {
-			animalId: { type: String, index: true },
+			animalId: { type: String, index: true, id: true },
 			name: { type: String, index: true },
 			sex: { type: String, enum: [ 'male', 'female', 'unknown' ] },
 			description: { type: String, index: true },
@@ -21,13 +21,19 @@ describe('ElasticsearchModel', function() {
 		return Animal.initialize().then(() => console.dir(Animal))
 			.then(() => {
 				let dog = Animal.create({
-					animalId: 'dog',
+					animalId: 'dog-charles-barkley-male',
 					name: 'Charles Barkley',
 					sex: 'male',
-					description: 'an asshole.',
+					description: 'this dog does not exist',
 					found: new Date()
 				});
 				console.dir(dog);
+				return dog.save();
+			})
+			.then((dog) => {
+				const dataDog = dog.getData();
+				dataDog.name = 'Ein';
+				dataDog.description = 'what the hell is a data dog?';
 				return dog.save();
 			});
 	});
