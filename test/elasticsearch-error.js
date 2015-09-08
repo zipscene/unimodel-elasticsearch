3f75ac907854b757ec5ff3edec529574ab7658af
Andrew Oppenlander <andrew.oppenlander@zipscene.com>
2015-09-08 15:30:11 -0400

    Updated comment and added a test for ESError

---
+++ test/elasticsearch-error.js
@@ -0,0 +1,16 @@
+const chai = require('chai');
+chai.use(require('chai-as-promised'));
+const { expect } = chai;
+const XError = require('xerror');
+
+const { ElasticsearchError } = require('../lib');
+
+describe('ElasticsearchError', function() {
+
+	it('should parse HTTP status codes into proper XError codes', function() {
+		let err = ElasticsearchError.fromESError('409', 'DocumentAlreadyExistsException');
+		expect(err.code).to.equal(XError.ALREADY_EXISTS);
+		expect(err.data).to.equal('DocumentAlreadyExistsException');
+	});
+
+});
